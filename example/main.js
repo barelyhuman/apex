e97cc2d556fc8486e27799a80ef9d61e0cd8b274
sidgroks@gmail.com <sidgroks@gmail.com>
2020-07-18 23:03:48 +0530

    base setup

--- example/main.js
+++ example/main.js
@@ -1,5 +1,8 @@
 (function () {
   Apex({
     el: "#editor",
+    font: "Hack,monospace",
+    highlight: (code) =>
+      Prism.highlight(code, Prism.languages.js, "javascript"),
   });
 })();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
-function main(config) {
-  console.log(config);
+let config = {
+  highlight: false,
+  font: "monospace",
+};
+
+function main(_config) {
+  config = Object.assign({}, config, _config);
+  const container = document.querySelector(config.el);
+  addEditor(container);
+}
+
+function addEditor(toContainer) {
+  const textArea = document.createElement("textarea");
+  const preArea = document.createElement("pre");
+
+  configure(preArea);
+  syncAreas(textArea, preArea);
+
+  toContainer.appendChild(textArea);
+  toContainer.appendChild(preArea);
+}
+
+function configure(codePrinter) {
+  codePrinter.style.fontFamily = config.font;
+}
+
+function syncAreas(codeEditor, codePrinter) {
+  codeEditor.addEventListener("keyup", (e) => {
+    let _value = e.target.value;
+
+    if (config.highlight && typeof config.highlight === "function") {
+      _value = config.highlight(_value);
+    }
+
+    codePrinter.innerHTML = _value;
+  });
 }
 
 export default main;
