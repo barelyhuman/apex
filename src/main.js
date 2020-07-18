ff380922ed75c93497589a4b2548a3d89b421c79
sidgroks@gmail.com <sidgroks@gmail.com>
2020-07-18 23:20:20 +0530

    Mod: styling changes, todos
    - styled the texteditor for now
    - added todos

--- src/main.js
+++ src/main.js
@@ -14,12 +14,26 @@ function addEditor(toContainer) {
   const preArea = document.createElement("pre");
   const codeArea = document.createElement("code");
 
+  // TODO: hide text area;
+  visualiseTextArea(textArea);
+
   configure(preArea, codeArea);
+
   syncAreas(textArea, codeArea);
 
   preArea.appendChild(codeArea);
   toContainer.appendChild(textArea);
   toContainer.appendChild(preArea);
+}
+
+function visualiseTextArea(tarea) {
+  tarea.style.boxSizing = "border-box";
+  tarea.style.resize = "none";
+  tarea.style.border = "2px solid #000";
+  tarea.style.borderRadius = "4px";
+  tarea.style.outline = "#000";
+  tarea.style.padding = "10px";
+  tarea.style.width = "100%";
 }
 
 function configure(codePrinter, codeArea) {
@@ -28,6 +42,8 @@ function configure(codePrinter, codeArea) {
 }
 
 function syncAreas(codeEditor, codePrinter) {
+  //TODO: Add cursor to pre area
+
   codeEditor.addEventListener("keyup", (e) => {
     let _value = e.target.value;
 
