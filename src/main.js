70f49158cd622dc1262285059c30192acde2635f
Siddharth <ahoy@barelyhuman.dev>
2021-06-03 04:35:12 +0530

    feat: allow updating the code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 let config = {
   highlight: (code) => code,
-  font: "monospace",
+  font: 'monospace',
   fontSize: 24,
   tabSpace: 2,
   disabled: false,
-};
+  editor: null
+}
 
 const keyCodes = {
   TAB: 9,
@@ -12,23 +13,23 @@ const keyCodes = {
   CURSOR_RIGHT: 39,
   CURSOR_UP: 38,
   CURSOR_DOWN: 40,
-  SPACE: 32,
-};
+  SPACE: 32
+}
 
 const NONREACTIVE_KEYS = [
   keyCodes.CURSOR_LEFT,
   keyCodes.CURSOR_RIGHT,
   keyCodes.CURSOR_UP,
   keyCodes.CURSOR_DOWN,
-  keyCodes.SPACE,
-];
+  keyCodes.SPACE
+]
 
-let highlighter = false;
+let highlighter = false
 
-function main(_config) {
-  config = Object.assign({}, config, _config);
-  const container = config.el;
-  container.classList.add(config.className);
+function main (_config) {
+  config = Object.assign({}, config, _config)
+  const container = config.el
+  container.classList.add(config.className)
   container.style = `
     position: relative;
     text-align: left;
@@ -36,39 +37,45 @@ function main(_config) {
     padding: 0px;
     overflow: hidden;
     font-family:${config.font};
-    font-size:${config.fontSize + "px"};
+    font-size:${config.fontSize + 'px'};
     line-height:calc(${config.fontSize}px * 1.5);
-  `;
+  `
 
-  addEditor(container);
+  this.config = config
+  this.updateCode = function (code) {
+    this.config.editor.value = code
+    this.config.editor.dispatchEvent(new InputEvent('change'))
+  }
+
+  addEditor(container)
 }
 
-function addEditor(toContainer) {
-  const textArea = document.createElement("textarea");
-  const preArea = document.createElement("pre");
-  const codeArea = document.createElement("code");
+function addEditor (toContainer) {
+  const textArea = document.createElement('textarea')
+  const preArea = document.createElement('pre')
+  const codeArea = document.createElement('code')
 
   // TODO: hide text area;
-  visualiseTextArea(textArea);
+  visualiseTextArea(textArea)
 
-  configure(preArea);
+  configure(preArea)
 
-  codeArea.style.fontFamily = "inherit";
+  codeArea.style.fontFamily = 'inherit'
 
-  syncAreas(textArea, codeArea);
-
-  preArea.appendChild(codeArea);
-  toContainer.appendChild(textArea);
-  toContainer.appendChild(preArea);
+  syncAreas(textArea, codeArea)
+  config.editor = textArea
+  preArea.appendChild(codeArea)
+  toContainer.appendChild(textArea)
+  toContainer.appendChild(preArea)
 }
 
-function visualiseTextArea(tarea) {
-  tarea.placeholder = config.placeholder;
-  tarea.value = config.value;
-  tarea.autoCapitalize = "off";
-  tarea.autoComplete = "off";
-  tarea.autoCorrect = "off";
-  tarea.spellcheck = false;
+function visualiseTextArea (tarea) {
+  tarea.placeholder = config.placeholder
+  tarea.value = config.value
+  tarea.autoCapitalize = 'off'
+  tarea.autoComplete = 'off'
+  tarea.autoCorrect = 'off'
+  tarea.spellcheck = false
   tarea.style = `
       margin: 0px;
       border: 0px;
@@ -100,13 +107,13 @@ function visualiseTextArea(tarea) {
       -webkit-font-smoothing: antialiased;
       -webkit-text-fill-color: transparent;
       padding: 10px;
-  `;
+  `
 
-  tarea.disabled = config.disabled;
+  tarea.disabled = config.disabled
 }
 
-function configure(codeAreaContainer) {
-  codeAreaContainer.setAttribute("aria-hidden", true);
+function configure (codeAreaContainer) {
+  codeAreaContainer.setAttribute('aria-hidden', true)
   codeAreaContainer.style = `
     margin: 0px;
     border: 0px;
@@ -131,69 +138,77 @@ function configure(codeAreaContainer) {
     pointer-events: none;
     padding: 10px;
     overflow-x: auto;
-  `;
+  `
 }
 
-function syncAreas(codeEditor, codePrinterContainer) {
-  if (config.highlight && typeof config.highlight === "function") {
-    highlighter = true;
-    codeEditor.style.webkitTextFillColor = "transparent";
+function syncAreas (codeEditor, codePrinterContainer) {
+  if (config.highlight && typeof config.highlight === 'function') {
+    highlighter = true
+    codeEditor.style.webkitTextFillColor = 'transparent'
   } else {
-    highlighter = false;
+    highlighter = false
   }
 
-  codeEditor.addEventListener("keydown", (e) => {
-    const selStart = e.target.selectionStart;
+  codeEditor.addEventListener('change', (e) => {
+    if (!highlighter) {
+      codePrinterContainer.innerText = codeEditor.value
+    }
+
+    highlighter && highlightText(codeEditor, codePrinterContainer)
+  })
+
+  codeEditor.addEventListener('keydown', (e) => {
+    const selStart = e.target.selectionStart
 
     if (e.keyCode === keyCodes.TAB) {
-      e.preventDefault();
+      e.preventDefault()
 
-      const tabChars = " ".repeat(config.tabSpace);
+      const tabChars = ' '.repeat(config.tabSpace)
       e.target.value =
         e.target.value.substring(0, e.target.selectionStart) +
         tabChars +
-        e.target.value.substring(e.target.selectionEnd);
+        e.target.value.substring(e.target.selectionEnd)
 
-      e.target.selectionStart = selStart + tabChars.length;
-      e.target.selectionEnd = selStart + tabChars.length;
+      e.target.selectionStart = selStart + tabChars.length
+      e.target.selectionEnd = selStart + tabChars.length
     }
-  });
+  })
 
-  codeEditor.addEventListener("keyup", (e) => {
+  codeEditor.addEventListener('keyup', (e) => {
     if (NONREACTIVE_KEYS.indexOf(e.keyCode) > -1) {
-      return;
+      return
     }
 
     if (!highlighter) {
-      codePrinterContainer.innerText = codeEditor.value;
+      codePrinterContainer.innerText = codeEditor.value
     }
 
-    highlighter && highlightText(codeEditor, codePrinterContainer);
-    config.onChange && config.onChange(e.target.value);
-  });
+    highlighter && highlightText(codeEditor, codePrinterContainer)
+    config.onChange && config.onChange(e.target.value)
+  })
 
   if (!highlighter) {
-    codePrinterContainer.innerText = codeEditor.value;
+    codePrinterContainer.innerText = codeEditor.value
   }
 
-  highlighter && highlightText(codeEditor, codePrinterContainer);
+  highlighter && highlightText(codeEditor, codePrinterContainer)
 }
 
-function highlightText(editor, printArea) {
-  const _value = editor.value;
-  editor.innerHTML = _value;
+function highlightText (editor, printArea) {
+  const _value = editor.value
+  editor.innerHTML = _value
 
   useHighlighter(_value)
     .then((data) => {
-      printArea.innerHTML = data;
+      printArea.innerHTML = data
     })
     .catch((err) => {
-      console.log(err);
-    });
+      console.log(err)
+    })
 }
 
-async function useHighlighter(value) {
-  return config.highlight(value);
+async function useHighlighter (value) {
+  return config.highlight(value)
 }
 
-export default main;
+export default main
